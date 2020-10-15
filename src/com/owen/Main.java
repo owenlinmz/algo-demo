f07a20d113ccd687850650cf39b60c9fa8fd065e
owenlin <owenlin@canway.net>
2020-10-15 11:02:35 +0800

    mark 10.15

--- src/com/owen/Main.java
+++ src/com/owen/Main.java
@@ -18,19 +18,30 @@ public class Main {
 
         ListNode() {
         }
-
-        ;
     }
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(4);
+
         node1.next = node2;
         node2.next = node3;
-        node3.next = node4;
-        node4.next = null;
+        node3.next = null;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = null;
+
+        ListNode resultNode = mergeTwoLists(node1, node4);
+        while (resultNode != null) {
+            System.out.println(resultNode.val);
+            resultNode = resultNode.next;
+        }
+
+
     }
 
     public static ListNode getKthFromEnd(ListNode head, int k) {
@@ -49,23 +60,37 @@ public class Main {
         node.next = node.next.next;
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
         ListNode newNode = new ListNode();
-        while (l1 != null || l2 != null) {
-            if (newNode == null) {
-                newNode = new ListNode();
-            }
-            if (l1.val < l2.val) {
+        ListNode initNode = newNode;
+        while (true) {
+            if (l1 == null) {
+                newNode.val = l2.val;
+                newNode.next = l2.next;
+                break;
+            } else if (l2 == null) {
                 newNode.val = l1.val;
-                newNode.next = null;
+                newNode.next = l1.next;
+                break;
+            } else if (l1.val < l2.val) {
+                newNode.val = l1.val;
                 l1 = l1.next;
             } else {
                 newNode.val = l2.val;
-                newNode.next = null;
                 l2 = l2.next;
             }
-            newNode = null;
+            newNode = getNextNode(newNode);
         }
+        return initNode;
+    }
+
+    public static ListNode getNextNode(ListNode node) {
+        ListNode newNode = new ListNode();
+        node.next = newNode;
+        return newNode;
     }
 
     public static ListNode addNode(int val) {
