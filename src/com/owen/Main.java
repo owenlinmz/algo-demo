c4a7f945ee71b503241a7201c74581a7a0cd5652
owenlin <owenlin@canway.net>
2020-10-15 18:19:46 +0800

    mark2 10.15

--- src/com/owen/Main.java
+++ src/com/owen/Main.java
@@ -87,15 +87,27 @@ public class Main {
         return initNode;
     }
 
+    public static ListNode mergeTwoListRecursion(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoListRecursion(l2, l1.next);
+            return l1;
+        } else {
+            l2.next = mergeTwoListRecursion(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static ListNode getNextNode(ListNode node) {
         ListNode newNode = new ListNode();
         node.next = newNode;
         return newNode;
     }
-
-    public static ListNode addNode(int val) {
-        return new ListNode(val, null);
-    }
-
 
 }
